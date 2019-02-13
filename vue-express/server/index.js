d64e614a3527f38f8b0f3a7e6e46ec116fce0ed0
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-02-13 19:55:18 +0300

    flstk vue static folder

--- vue-express/server/index.js
+++ vue-express/server/index.js
@@ -13,4 +13,15 @@ app.use('/api/posts', posts)
 
 const port = process.env.PORT || 8000
 
+// Handle prod
+if (process.env.NODE_ENV === 'production' || 1) {
+    app.use(express.static(__dirname + '/public/'))
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html')
+    })
+}
+
+
 app.listen(port, () => console.log(`Server started on ${port}`))
