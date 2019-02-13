3a6d730bff03c6630e7d1dbfef89e2fe9834e6e1
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-02-13 19:46:34 +0300

    flsk vue client

---
+++ vue-express/client/src/PostService.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const url = 'http://localhost:8000/api/posts/'
+
+class PostService {
+    static getPost() {
+        return new Promise(async (resolve, rej) => {
+            try{
+                const res = await axios.get(url)
+                const data = res.data
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                )
+            } catch(err) {
+                rej(err)
+            }
+        })
+    }
+
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        })
+    }
+
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`)
+    }
+}
+
+export default PostService
